e527deadbc6e8ae12bc9a65f8481aeef70731565
gil-cesar-martins <gilcesartst@gmail.com>
2023-10-08 12:17:55 -0300

    version 002

--- app.py
+++ app.py
@@ -12,13 +12,28 @@ import matplotlib
 matplotlib.use('Agg')
 import altair as alt
 
+# Utils
+@st.cache_resource
+def load_bible(data):
+    df = pd.read_csv(data)
+    return df 
+
 def main():
+    
     st.title("📖 Estudo Bíblico - Streamlit 📖")
     menu = ["Home", "Multiverse", "Sobre"]
+    
+    df = load_bible("data\KJV_BIble.csv")
     
     choice = st.sidebar.selectbox("Menu",menu)
     if choice == "Home":
         st.subheader("Buscar um único versículo")
+        st.dataframe(df)
+        book_list = df['book'].unique().tolist()
+        book_name = st.sidebar.selectbox("Book", book_list)
+        chapter = st.sidebar.number_input("Chapter",1)
+        verse = st.sidebar.number_input("Verse",1)
+        bilble_df = df['book'] == book_name
     elif choice == "Multiverse":
         st.subheader("Buscar múltiplos versículos")
     else:
