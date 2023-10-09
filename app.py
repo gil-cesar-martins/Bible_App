7715c7cd509d855c07c7c5c47a574341efac59b5
gil-cesar-martins <gilcesartst@gmail.com>
2023-10-09 11:13:24 -0300

    version 003

--- app.py
+++ app.py
@@ -28,12 +28,33 @@ def main():
     choice = st.sidebar.selectbox("Menu",menu)
     if choice == "Home":
         st.subheader("Buscar um único versículo")
-        st.dataframe(df)
+        #st.dataframe(df)
         book_list = df['book'].unique().tolist()
         book_name = st.sidebar.selectbox("Book", book_list)
         chapter = st.sidebar.number_input("Chapter",1)
         verse = st.sidebar.number_input("Verse",1)
-        bilble_df = df['book'] == book_name
+        bilble_df = df[df['book'] == book_name]
+        #st.dataframe(bilble_df)
+        
+        # Layout
+        c1,c2 = st.columns([2,1])
+        
+        # Single Verse layout
+        with c1:
+            try:
+                selected_passage = bilble_df[(bilble_df['chapter'] == chapter) & (bilble_df['verse'] == verse)]
+                #st.write(selected_passage)
+                passage_details = "{} Chapter::{} Verse::{}".format(book_name,chapter,verse)
+                st.info(passage_details)
+                passage = "{}".format(selected_passage['text'].values[0])
+                st.write(passage)
+            except:
+                st.warning("Book out of range")
+            
+        with c2:
+            st.success("Verse of the Day.")
+        # Verse of the Day
+        
     elif choice == "Multiverse":
         st.subheader("Buscar múltiplos versículos")
     else:
