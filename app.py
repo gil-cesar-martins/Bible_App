a4328e000f91623b710971104468737a47410528
gil-cesar-martins <gilcesartst@gmail.com>
2023-10-12 10:19:32 -0300

    version 004

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import streamlit.components.v1 as stc
 # EDA Packages
 import pandas as pd
 import neattext.functions as nfx 
+import random
 
 # Data Viz Packages
 import matplotlib.pyplot as plt
@@ -17,6 +18,8 @@ import altair as alt
 def load_bible(data):
     df = pd.read_csv(data)
     return df 
+
+from utils import HTML_RANDOM_TEMPLATE
 
 def main():
     
@@ -52,9 +55,30 @@ def main():
                 st.warning("Book out of range")
             
         with c2:
-            st.success("Verse of the Day.")
-        # Verse of the Day
-        
+            st.success("Verse of the Day")
+            chapter_list = range(10)
+            verse_list = range(20)
+            ch_choice = random.choice(chapter_list)
+            vs_choice = random.choice(verse_list)
+            random_book_name = random.choice(book_list)
+            
+            #st.write("Livro: {}, Cap: {}, Vers: {}".format(random_book_name,ch_choice,vs_choice))
+            rand_bilble_df = df[df['book'] == random_book_name]
+           
+            try:
+                randomly_selected_passage = rand_bilble_df[(rand_bilble_df['chapter'] == ch_choice) & (rand_bilble_df['verse'] == vs_choice)]
+                my_text = randomly_selected_passage['text'].values[0]
+            except:
+                my_text = rand_bilble_df[(rand_bilble_df['chapter'] == 1) & (rand_bilble_df['verse'] == 1)]['text'].values[0]
+              
+            stc.html(HTML_RANDOM_TEMPLATE.format(my_text), height=300)
+            
+        # Search topic/term
+        search_term = st.text_input("Term/Topic")
+        with st.expander("View Results"):
+            retrived_df = df[df['text'].str.contains(search_term)]
+            st.dataframe(retrived_df[['book','chapter','verse','text']])
+                    
     elif choice == "Multiverse":
         st.subheader("Buscar múltiplos versículos")
     else:
---
+++ utils.py
@@ -0,0 +1,9 @@
+HTML_RANDOM_TEMPLATE = """
+<div style='padding:10px;background-color: rgb(167,204,238);
+border-radius: 8px 34px 9px 26px;
+-moz-border-radius: 8px 34px 9px 26px;
+-webkit-border-radius: 8px 34px 9px 26px;
+border: 2px ridge #000000;'>
+    <h5>👩🏻‍💼 Verse of the Day ❤️</h5>
+    <p>{}</p>
+</div>"""
